a15f7b37dcbe715e92e8e037cdc41524fc09080a
Dwi Susanto <dwis190@gmail.com>
2023-09-03 22:02:17 +0700

    Aplikasi LoundryApp Versi 1.0

--- LoundryApp/main.go
+++ LoundryApp/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"LoundryApp/config"
-	"LoundryApp/controllers"
+	"log"
+
+	"LoundryApp/controllers/CustomersControllers"
+	"LoundryApp/controllers/TransaksiControllers"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,13 +12,16 @@ import (
 func main() {
 	r := gin.Default()
 
-	// Inisialisasi koneksi ke database
-	models.DB = config.DBInit()
-	defer models.DB.Close()
+	// Route untuk customers
+	r.POST("/customers", CustomersControllers.TambahCustomer)
+	r.PUT("/customers/:id", CustomersControllers.UpdateCustomer)
+	r.DELETE("/customers/:id", CustomersControllers.HapusCustomer)
 
-	r.POST("/tambah-customer", controllers.TambahCustomer)
-	r.PUT("/update-customer/:id", controllers.UpdateCustomer)
-	r.DELETE("/hapus-customer/:id", controllers.HapusCustomer)
+	// Route untuk transactions
+	r.POST("/transactions", TransaksiControllers.TambahTransaksi)
 
-	r.Run(":8080") // Ganti port sesuai kebutuhan Anda
+	// Jalankan server pada port 8080
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal("Failed to start server: ", err)
+	}
 }
